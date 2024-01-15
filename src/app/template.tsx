e75604e7207622d6599a222722d23aed848a1751
Kamil <kamil.niftaliev@gmail.com>
2024-01-15 20:27:25 +0400

    style: content height on desktop

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -4,7 +4,7 @@ import { PropsWithChildren } from "react";
 
 export default function Template({ children }: PropsWithChildren) {
   return (
-    <main className="flex items-center justify-center grow h-content">
+    <main className="flex items-center justify-center grow h-content sm:h-screen">
       {children}
     </main>
   );
